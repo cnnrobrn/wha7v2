bb5df5a3736a16aaa047470b7ea89d27c549ad06
cnnrobrn <connorpallavobrien@gmail.com>
2024-11-17 17:50:14 -0800

    modified:   .env modified:   app.py modified:   requirements.txt

--- app.py
+++ app.py
@@ -15,6 +15,8 @@ from sqlalchemy import create_engine, Column, Integer, String, ForeignKey
 from sqlalchemy.orm import relationship, sessionmaker
 import psycopg2
 from flask_migrate import Migrate, upgrade
+import re
+from pprint import pprint
 
 
 app = Flask(__name__)
@@ -26,11 +28,9 @@ load_dotenv()
 TWILIO_ACCOUNT_SID=os.getenv('TWILIO_ACCOUNT_SID')
 TWILIO_AUTH_TOKEN=os.getenv('TWILIO_AUTH_TOKEN')
 API_KEY=os.getenv('OPENAI_API_KEY')
-EBAY_AFFILIATE_ID = os.getenv("EBAY_AFFILIATE_ID")
-EBAY_APP_ID = os.getenv("EBAY_APP_ID")
-EBAY_DEV_ID = os.getenv("EBAY_DEV_ID")
-EBAY_CERT_ID = os.getenv("EBAY_CERT_ID")
 DATABASE_URL= os.getenv("DATABASE_URL")
+OXY_USERNAME= os.getenv("OXY_USERNAME")
+OXY_PASSWORD= os.getenv("OXY_PASSWORD")
 
 app.config['SQLALCHEMY_DATABASE_URI'] = DATABASE_URL
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -51,20 +51,27 @@ class Outfit(db.Model):
     image_data = db.Column(db.Text, nullable=True)  # New column to store the base64 encoded image
     description = db.Column(db.String(1000), nullable=False)
     items = db.relationship('Item', backref='outfit', lazy=True)
-
-
+    
 class Item(db.Model):
     __tablename__ = 'items'
     id = db.Column(db.Integer, primary_key=True)
     outfit_id = db.Column(db.Integer, db.ForeignKey('outfits.id'), nullable=False)
+    description = db.Column(db.Text, nullable=True)  # Change to Text to handle longer descriptions
+    links = db.relationship('Link', backref='item', lazy=True)
+
+class Link(db.Model):
+    __tablename__ = 'links'
+    id = db.Column(db.Integer, primary_key=True)
+    item_id = db.Column(db.Integer, db.ForeignKey('items.id'), nullable=False)
+    photo_url = db.Column(db.Text, nullable=True)  # Increased to accommodate long photo URLs
     url = db.Column(db.String(2000), nullable=False)  # Increased to accommodate longer URLs
     price = db.Column(db.String(200), nullable=True)  # Increase length if needed
-    ebay_short_description = db.Column(db.Text, nullable=True)  # Change to Text to handle longer descriptions
-    photo_url = db.Column(db.String(2000), nullable=True)  # Increased to accommodate long photo URLs
+    title = db.Column(db.String(2000), nullable=False)  # Increased to accommodate longer URLs
+    rating = db.Column(db.Float, nullable=True)
+    reviews_count = db.Column(db.Integer, nullable=True)
+    merchant_name = db.Column(db.String(200), nullable=True)
 
 
-
-streamlit_data = {}
 
 migrate = Migrate(app, db)
 
@@ -80,15 +87,6 @@ class Outfits(BaseModel):
 
 
 EBAY_ENDPOINT = "https://api.ebay.com/buy/browse/v1/item_summary/search?q="
-
-
-@app.route("/clothes", methods=['POST'])
-def clothes():
-    from_number = request.form.get('From')
-    if from_number in streamlit_data:
-        return json.dumps(streamlit_data[from_number])
-    else:
-        return json.dumps({"clothes": []})
 
 prompt = """You are the best fashion and accessories finder in the world. When people share photos of outfits with you, you identify each individual item in the outfit—including clothing and accessories—with as much detail as possible. For each item, you provide:
 
@@ -141,6 +139,7 @@ client = OpenAI()
 
 @app.route("/sms", methods=['POST'])
 def sms_reply():
+    db.create_all()
     # Extract incoming message information
     from_number = request.form.get('From')
     media_url = request.form.get('MediaUrl0')  # This will be the first image URL
@@ -152,61 +151,28 @@ def sms_reply():
             base64_image = base64.b64encode(image_content).decode('utf-8')
             base64_image_data = f"data:image/jpeg;base64,{base64_image}"
             clothing_items = analyze_image_with_openai(base64_image_data)
+
             
-            # Search for eBay links
-            ebay_access_token = ebay_oauth_flow()
-            links = {}
-            images = {}
-            shortDescriptions = {}
-            prices = {}
+            database_commit(clothing_items, from_number, base64_image_data)
             
             for item in clothing_items.Article:
-                ebay_list = search_ebay(item.Amazon_Search, ebay_access_token)
-                links[item.Amazon_Search] = ebay_list['links'] 
-                images[item.Amazon_Search+"_image"] = ebay_list['images']    
-                shortDescriptions[item.Amazon_Search+"_shortdescription"] = ebay_list['shortDescription']    
-                prices[item.Amazon_Search+"_price"] = ebay_list['price']   
-            
-            # Save to PostgreSQL
-            # Create or get the PhoneNumber
-            phone = PhoneNumber.query.filter_by(phone_number=from_number).first()
-            if not phone:
-                phone = PhoneNumber(phone_number=from_number)
-                db.session.add(phone)
-                db.session.commit()
+                results = oxy_search(item.Amazon_Search)
+                item_id=Item.query.filter_by(description=item.Item).first()
+                if item_id:
+                    for result in results['results']:
+                        new_link = Link(item_id=item_id.id, url=result['url'], title=result['title'], photo_url=result['thumbnail'], price=result['price'], rating=result['rating'],reviews_count=result['reviews_count'],merchant_name=result['merchant_name'])
+                        db.session.add(new_link)
+                        db.session.commit()
 
-            # Create a new Outfit
-            outfit = Outfit(phone_id=phone.id,image_data=base64_image_data ,description="Outfit from image")
-            db.session.add(outfit)
-            db.session.commit()
 
             # Add Items to the outfit
-            for (item, urls), (desc, images), (detail, desc_values), (pic, prices) in zip(links.items(), images.items(), shortDescriptions.items(), prices.items()):
-                for url, image, desc_value, price in zip(urls, images, desc_values, prices):
-                    new_item = Item(
-                        outfit_id=outfit.id,
-                        url=url,
-                        price=price,
-                        ebay_short_description=desc_value,
-                        photo_url=image
-                    )
-                    db.session.add(new_item)
+                    # db.session.add(new_item)
             
-                    db.session.commit()
+                    # db.session.commit()
 
             # Construct response message
             resp = MessagingResponse()
-            resp.message("Access all the outfits you've shared at feed.wha7.com")
-            for item, urls in links.items():
-                message = f"Top links for {item}:\n" 
-                for url in urls:
-                    short_url = shorten_url(url)
-                    message += short_url + "\n"
-                try:
-                    resp.message(message)
-                except Exception as e:
-                    print(f"Error sending message: {e}")
-                    resp.message("An error occurred while processing your request.")
+            resp.message("Added! Access all the outfits you've shared at feed.wha7.com")
             return str(resp)
         else:
             return "Error: Unable to fetch the image."
@@ -248,72 +214,43 @@ def analyze_image_with_openai(base64_image):
         print(f"Error analyzing image with OpenAI: {e}")
         return None
 
-def search_amazon(query,ebay_access_token):
-    results= [f"https://www.amazon.com/s?k={query}&page={i}" for i in range(1, 4)]
-    print(results)
-    return results
-
-
-def ebay_oauth_flow():
-    headers = {
-        "Content-Type": "application/x-www-form-urlencoded",
-        "Authorization": f"Basic {base64.b64encode((EBAY_APP_ID + ':' + EBAY_CERT_ID).encode()).decode()}"
-    }
-    data = {
-        "grant_type": "client_credentials",
-        "scope": "https://api.ebay.com/oauth/api_scope"
+def oxy_search(query):
+    # Structure payload.
+    payload = {
+    'source': 'google_shopping_search',
+    'parse': True,
+    'query': query
     }
 
-    response = requests.post("https://api.ebay.com/identity/v1/oauth2/token", headers=headers, data=data)
+    # Get response.
+    response = requests.request(
+        'POST',
+        'https://realtime.oxylabs.io/v1/queries',
+        auth=(OXY_USERNAME, OXY_PASSWORD), #Your credentials go here
+        json=payload,
+    )
 
-    if response.status_code == 200:
-        token_data = response.json()
-        access_token = token_data.get("access_token")
-        #print(f"Obtained eBay OAuth token: {access_token}")
-        return access_token
-    else:
-        print(f"Failed to obtain eBay OAuth token: {response.status_code} - {response.text}")
-        return None   
-    
-def search_ebay(query,ebay_access_token):
-    try:
-        headers = {
-            "Authorization": f"Bearer {ebay_access_token}",
-            "Content-Type": "application/json"
-        }
-        #print(ebay_access_token)
-        url_query=urllib.parse.quote_plus(query)
-        endpoint = f"{EBAY_ENDPOINT}{url_query}&limit=3&filter=buyingOptions:{{'FIXED_PRICE'}}&filter=deliveryCountry:US,conditions:{{'NEW'}}"
-        response = requests.get(
-            endpoint,
-            headers=headers
-        )
-        links={}
-        response.raise_for_status()  # Raise an HTTPError for bad responses
-        response_data = response.json()
-        items = response_data.get("itemSummaries", [])
-        links['links'] = [item.get("itemWebUrl") for item in items]
-        links['images'] = [item.get("image").get("imageUrl") for item in items]
-        links['shortDescription'] = [item.get("title", "") for item in items]
-        links['price'] = [item.get("price", {}).get("value", "") for item in items]
-        return links
-    except requests.exceptions.HTTPError as http_err:
-        print(f"HTTP error occurred: {http_err}")
-        return ["HTTP error occurred"]
-    except Exception as err:
-        print(f"Other error occurred: {err}")
-        return ["An error occurred"]
-
-# Flask route to retrieve the original URL and redirect
-@app.route('/<short_code>', methods=['GET'])
-def retrieve(short_code):
-    original_url = retrieve_original_url(short_code)
-    if original_url:
-        return redirect(original_url)
-    else:
-        return jsonify({'error': 'Short code not found'}), 404
+    # Instead of response with job status and results url, this will return the
+    # JSON response with results.
+    data = response.json()
+    organic_results = data["results"][0]["content"]["results"]["organic"][:30]
 
 
+    structured_data = [
+    {
+        "pos": result.get("pos"),
+        "price": result.get("price"),
+        "title": result.get("title"),
+        "thumbnail": result.get("thumbnail"),
+        "url": result.get("url"),
+        "rating": result.get("rating"),
+        "reviews_count": result.get("reviews_count"),
+        "merchant_name": result.get("merchant", {}).get("name")
+    }
+    for result in organic_results
+    ]
+
+    return structured_data
 
 def shorten_url(long_url):
     # Define the endpoint URL (change port if necessary)
@@ -334,7 +271,31 @@ def shorten_url(long_url):
     else:
         print('Error:', response.json().get('error'))
         return None
+    
+def database_commit(clothing_items, from_number, base64_image_data):
+    # Extract the items from the parsed response
+            # Save to PostgreSQL
+            # Create or get the PhoneNumber
+    phone = PhoneNumber.query.filter_by(phone_number=from_number).first()
+    if not phone:
+        phone = PhoneNumber(phone_number=from_number)
+        db.session.add(phone)
+        db.session.commit()
+
+        # Create a new Outfit
+    outfit = Outfit(phone_id=phone.id,image_data=base64_image_data ,description="Outfit from image")
+    db.session.add(outfit)
+    db.session.commit()
+            
+    for item in clothing_items.Article:
+        new_item = Item(outfit_id=outfit.id, description=item.Item)
+        db.session.add(new_item)
+        db.session.commit()
+        # for link in item['Amazon_Search']:
+        #     new_link = Link(item_id=new_item.id, url=link['url'], title=link['title'], photo_url=link['photo_url'], price=link['price'])
+        #     db.session.add(new_link)
+        #     db.session.commit()
+    return None
 
 if __name__ == "__main__":
-    db.create_all()
     app.run(host="0.0.0.0", port=5000, debug=True)
