ff4febb2dcd14983cfca060cbf325cf82a49209e
cnnrobrn <141182288+cnnrobrn@users.noreply.github.com>
2024-11-25 22:15:55 -0800

    modified:   app.py

--- app.py
+++ app.py
@@ -340,7 +340,7 @@ def process_response(base64_image,from_number,text,prompt_text=prompt,format=Out
         clothing_items = analyze_image_with_openai(base64_image_data,text,prompt_text,format)
         database_commit(clothing_items, from_number, base64_image_data)
     else:
-        clothing_items = analyze_text_with_openai(text=text,prompt=prompt_text,format=format)      
+        clothing_items = analyze_text_with_openai(text=text,true_prompt=prompt_text,format=format)      
     return clothing_items
 def shorten_url(long_url):
     # Define the endpoint URL (change port if necessary)
