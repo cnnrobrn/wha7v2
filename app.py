5b1192f916000610447bf6791d12e0de5bf32b8a
cnnrobrn <141182288+cnnrobrn@users.noreply.github.com>
2024-11-25 21:01:51 -0800

    modified:   app.py

--- app.py
+++ app.py
@@ -250,7 +250,8 @@ def analyze_image_with_openai(base64_image,text):
         return None
 
 def process_response(base64_image,from_number,text):
-    base64_image_data = f"data:image/jpeg;base64,{base64_image}"
+    if base64_image:
+        base64_image_data = f"data:image/jpeg;base64,{base64_image}"
     clothing_items = analyze_image_with_openai(base64_image_data,text)        
     database_commit(clothing_items, from_number, base64_image_data)
     return clothing_items
